381cbfa10b110e21dac954a9dd86f3d17f4b952c
Akgun D <dikkanakgun@gmail.com>
2022-10-18 17:50:37 +0300

    flask-01 and flask-02 added.

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return "<h1>Hello World!</h1>"
+
+@app.route('/second')
+def second():
+    return 'Bize her yer Trabzon!!!'
+
+@app.route('/third/subthird')
+def third():
+    return '''This is the subpage of Akgun's third page.'''
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'
+
+if __name__ == "__main__":
+    app.run(debug=True, port:2000)
